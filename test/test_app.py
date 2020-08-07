c5d7627aa1cbd748d5a728af8eda0768941f6f70
Akio <rsalunga29@gmail.com>
2020-08-07 12:30:18 +0800

    test pull request checks

---
+++ test/test_app.py
@@ -0,0 +1,7 @@
+def test_index(app, client):
+    res = client.get('/')
+    assert res.status_code == 200
+
+    expected = 'This is a CI/CD Pipeline with Kubernetes test. \n Im making changes too!'
+    assert expected == 'This is a CI/CD Pipeline with Kubernetes test. \n Im making changes too!'
+
