b99274102571744660b6d1e12046288ba16e38aa
Akio <rsalunga29@gmail.com>
2020-08-07 12:45:32 +0800

    fix unit tests

--- test/test_app.py
+++ test/test_app.py
@@ -3,5 +3,4 @@ def test_index(app, client):
     assert res.status_code == 200
 
     expected = 'This is a CI/CD Pipeline with Kubernetes test. \n Im making changes too!'
-    assert expected == 'This is a CI/CD Pipeline with Kubernetes test. \n Im making changes too!'
-
+    assert expected == res.get_data(as_text=True)
